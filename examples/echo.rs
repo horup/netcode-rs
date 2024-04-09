f9b10f6263b2b0a6eae7bcce1c9e046a3e216426
Søren Alsbjerg Hørup <sah@sah.dk>
2024-04-09 12:55:13 +0200

    echo now runs one iteration

--- examples/echo.rs
+++ examples/echo.rs
@@ -1,13 +1,13 @@
 use std::time::Duration;
 /// echo server and client example
 /// 
-/// client connects to the server, sends a message every second
+/// clients connects to the server, sends a message every second
 /// 
 /// server replys with a copy of the message.
 #[tokio::main]
 pub async fn main() {
     // spawn a server
-    tokio::spawn(async {
+    let server_handle = tokio::spawn(async {
         use netcode::server::*;
         let mut server = Server::default() as Server<String>;
         server.start(8080).await;
@@ -21,24 +21,35 @@ pub async fn main() {
         }
     });
 
-    // spawn client
-    let _ = tokio::spawn(async {
-        use netcode::client::*;
-        let mut client = Client::default() as Client<String>;
-        client.connect("ws://localhost:8080").await;
-        loop {
-            for e in client.poll() {
-                if let Event::Message(msg) = &e {
-                    println!("{}", msg);
+    let mut clients = Vec::default();
+    // spawn clients
+    for i in 0..10 {
+        let j = tokio::spawn(async move {
+            use netcode::client::*;
+            let mut client = Client::default() as Client<String>;
+            client.connect("ws://localhost:8080").await;
+            loop {
+                for e in client.poll() {
+                    if let Event::Message(msg) = &e {
+                        println!("{}", msg);
+                        client.disconnect().await;
+                    }
                 }
-            }
 
-            if client.state() == State::Connected {
-                client.send("hello world".to_owned());
-            }
+                if client.state() == State::Connected {
+                    client.send(format!("hello from {}", i));
+                }
 
-            tokio::time::sleep(Duration::from_millis(1000)).await;
-        }
-    })
-    .await;
+                tokio::time::sleep(Duration::from_millis(1000)).await;
+            }
+        });
+        clients.push(j);
+    }
+
+    for client in clients {
+        let _ = client.await;
+    }
+    
+    dbg!("all done");
+    server_handle.abort();
 }
