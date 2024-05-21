d85710ea757e66b4190906a6c943e654d6b030d5
Søren Alsbjerg Hørup <sah@sah.dk>
2024-05-21 07:56:34 +0200

    wip master example

--- examples/master.rs
+++ examples/master.rs
@@ -1,7 +1,17 @@
 //! example with a master server, some instances and some clients
 
 use master_server::MasterServer;
-fn main() {
-    let mut master_server:MasterServer<()> = Default::default();
-    
+use server::Server;
+#[tokio::main]
+async fn main() {
+    let server = async {
+        let mut server = Server::default();
+        server.start(8080).await;
+        let mut master_server:MasterServer<()> = MasterServer::new(server);
+        
+    };
+
+    let server_handle = tokio::spawn(server);
+
+    let _ = server_handle.await;
 }
--- master_server/src/lib.rs
+++ master_server/src/lib.rs
@@ -5,20 +5,18 @@ use server::Server;
 /// Hosts a single `server` acting as master for instances.
 /// 
 /// A client initially connects to the master and then through the master is directed to an instance.
-#[derive(Default)]
 pub struct MasterServer<T> where T:SerializableMessage {
     pub server:Server<T>,
     pub instances:HashMap<InstanceId, Instance<T>>
 }
 
 impl<T:SerializableMessage> MasterServer<T> {
-    /// Listens on the specific port
-    ///
-    /// Returns `false` if port cannot be acquired
-    pub async fn start(&mut self, port: u16) -> bool {
-        self.server.start(port).await
+    pub fn new(server:Server<T>) -> Self {
+        Self {
+            server,
+            instances:Default::default()
+        }
     }
-
     pub fn pool(&mut self) {
         
     }
